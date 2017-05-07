51bea1fe238ac127046d4b56e36410d0b8b70d30
Aaron McDaid <aaronmcdaid@gmail.com>
2017-05-07 20:49:46 +0200

    Complete the non-macro portion of the simpler method

--- IF_CONSTEXPR.hh
+++ IF_CONSTEXPR.hh
@@ -4,8 +4,3 @@
 #define FIRST_EXPRESSION(e1) ( [&] () { return e1; } , SECOND_EXPRESSION
 #define SECOND_EXPRESSION(e2)   [&] () { return e2; } ) ()
 
-
-
-#define IF_CONSTEXPR(cond) if_constexpr(cond) CAPTURE_TWO_VARS
-#define CAPTURE_TWO_VARS(v1,v2) [&v1,&v2]() { TWO_EXPRESSIONS
-#define TWO_EXPRESSIONS(e1,e2) return if_constexpr<cOnD>(  [&](){return e1;} , [&](){return e1;} )
--- demo.cc
+++ demo.cc
@@ -5,20 +5,47 @@
  */
 #include <iostream>
 #include <type_traits>
+#include <tuple>
 #include "IF_CONSTEXPR.hh"
 
-template<typename T, typename F, typename ...Args>
-auto if_c(std:: true_type, T&& t, F&&, Args&&... args)
-{
-    return std::forward<T>(t)( std::forward<Args>(args) ... );
-}
-template<typename T, typename F, typename ...Args>
-auto if_c(std:: false_type, T&&, F&&f, Args&&... args)
-{
-    return std::forward<F>(f)( std::forward<Args>(args) ... );
+template<size_t ... I, typename F>
+decltype(auto) apply_integer_sequence(F &&f, std:: index_sequence<I...>) {
+    return std::forward<F>(f) ( std::integral_constant<size_t, I>{} ... );
 }
 
+template<size_t count, typename F>
+decltype(auto) apply_integer_sequence(F&&f) {
+    return apply_integer_sequence
+        (   std::forward<F>(f)
+        ,   std::make_index_sequence<count>{}
+        );
+}
 
+template<typename ... Args>
+struct forward_as_tuple_for_if_constexpr_helper {
+    std::tuple< Args ...> m_args;
+    forward_as_tuple_for_if_constexpr_helper(Args&& ...args) : m_args( std::forward<Args>(args)... ){}
+
+    template<typename T, typename F>
+    decltype(auto) if_constexpr(std:: false_type, T&&, F&&f)
+    {
+        return apply_integer_sequence<sizeof...(Args)>( [&](auto &&... idxs){
+                return std::forward<F>(f)( std::get< std::remove_reference_t<decltype(idxs)>::value >(m_args) ...);
+        });
+    }
+    template<typename T, typename F>
+    decltype(auto) if_constexpr(std::  true_type, T&&t, F&&)
+    {
+        return apply_integer_sequence<sizeof...(Args)>( [&](auto &&... idxs){
+                return std::forward<T>(t)( std::get< std::remove_reference_t<decltype(idxs)>::value >(m_args) ...);
+        });
+    }
+};
+
+template<typename ... Args>
+auto forward_as_tuple_for_if_constexpr(Args&& ... args) {
+    return forward_as_tuple_for_if_constexpr_helper<Args...>( std::forward<Args>(args)... );
+}
 
 int main() {
     int a=3;
@@ -27,17 +54,26 @@ int main() {
     std:: cout << IF_CONSTEXPR_SIMPLE(true ) ( a*b ) (a+b) << '\n';
     std:: cout << IF_CONSTEXPR_SIMPLE(false) ( a*b ) (a+b) << '\n';
 
-    // IF_CONSTEXPR( true )( a, b,
+    // IF_CONSTEXPR( a, b, true,
     //       a*b
     // )(
     //       a+b
     // )
 
-    auto x =
-    if_c( std::integral_constant<bool, false>{}
-           ,[&](auto&&a,auto&&b){ return a*b; }
-           ,[&](auto&&a,auto&&b){ return a+b; }
-           ,a,b
-           );
-    std::cout << x <<'\n';
+    std::cout
+        <<  forward_as_tuple_for_if_constexpr(a,b)
+            .if_constexpr(
+                    std::integral_constant<bool, true>{}
+                ,   [&](auto&&a,auto&&b){ return a*b; }
+                ,   [&](auto&&a,auto&&b){ return a(b); }
+               )
+        <<'\n';
+    std::cout
+        <<  forward_as_tuple_for_if_constexpr(a,b)
+            .if_constexpr(
+                    std::integral_constant<bool, false>{}
+                ,   [&](auto&&a,auto&&b){ return a(b); }
+                ,   [&](auto&&a,auto&&b){ return a+b; }
+               )
+        <<'\n';
 }
