d01cdf318f6ccea51eabeccdf484e34bb945d641
Aaron McDaid <aaronmcdaid@gmail.com>
2017-05-07 18:58:33 +0200

    Start a much simpler system, using a comma in place of one ')('

--- demo.cc
+++ demo.cc
@@ -4,7 +4,20 @@
  * project is all about
  */
 #include <iostream>
+#include <type_traits>
 #include "IF_CONSTEXPR.hh"
+
+template<typename T, typename F, typename ...Args>
+auto if_c(std:: true_type, T&& t, F&&, Args&&... args)
+{
+    return std::forward<T>(t)( std::forward<Args>(args) ... );
+}
+template<typename T, typename F, typename ...Args>
+auto if_c(std:: false_type, T&&, F&&f, Args&&... args)
+{
+    return std::forward<F>(f)( std::forward<Args>(args) ... );
+}
+
 
 
 int main() {
@@ -13,4 +26,18 @@ int main() {
 
     std:: cout << IF_CONSTEXPR_SIMPLE(true ) ( a*b ) (a+b) << '\n';
     std:: cout << IF_CONSTEXPR_SIMPLE(false) ( a*b ) (a+b) << '\n';
+
+    // IF_CONSTEXPR( true )( a, b,
+    //       a*b
+    // )(
+    //       a+b
+    // )
+
+    auto x =
+    if_c( std::integral_constant<bool, false>{}
+           ,[&](auto&&a,auto&&b){ return a*b; }
+           ,[&](auto&&a,auto&&b){ return a+b; }
+           ,a,b
+           );
+    std::cout << x <<'\n';
 }
