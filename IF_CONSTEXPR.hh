2cba4f7c5a5b17d4339e2720fff2ae32d21d8fad
Aaron McDaid <aaronmcdaid@gmail.com>
2017-05-08 01:53:45 +0200

    Use COUNT_THE_ARGS_AND_THEN_CALL_THE_APPROPRIATE_OVERLOAD for both REVERSE and ALL_BUT_TWO_VARS_AS_GENERIC_LAMBDA_ARGS

--- IF_CONSTEXPR.hh
+++ IF_CONSTEXPR.hh
@@ -31,7 +31,6 @@
 #define                          LAST_MACRO_ARG(...)       EVAL(          FIRST_MACRO_ARG                REVERSE(__VA_ARGS__)  )
 #define                   SECOND_LAST_MACRO_ARG(...)       EVAL(          SECOND_MACRO_ARG               REVERSE(__VA_ARGS__)  )
 #define            DROP_THE_LAST_TWO_MACRO_ARGS(...)  EVAL(EVAL(  REVERSE DROP_THE_FIRST_TWO_MACRO_ARGS  REVERSE(__VA_ARGS__)  ))
-#define ALL_BUT_TWO_VARS_AS_GENERIC_LAMBDA_ARGS(...) PASTE(ALL_BUT_TWO_VARS_AS_GENERIC_LAMBDA_ARGS_, COUNT_MACRO_ARGS(__VA_ARGS__))(__VA_ARGS__)
 
 #define EVAL(f, ...) f __VA_ARGS__
 
@@ -39,11 +38,14 @@
 #define                        SECOND_MACRO_ARG(arg1, arg2, ...)  arg2
 #define           DROP_THE_FIRST_TWO_MACRO_ARGS(arg1, arg2, ...) (__VA_ARGS__)
 
+#define REVERSE(...)                                    COUNT_THE_ARGS_AND_THEN_CALL_THE_APPROPRIATE_OVERLOAD(REVERSE_                                  , __VA_ARGS__)
+#define ALL_BUT_TWO_VARS_AS_GENERIC_LAMBDA_ARGS(...)    COUNT_THE_ARGS_AND_THEN_CALL_THE_APPROPRIATE_OVERLOAD(ALL_BUT_TWO_VARS_AS_GENERIC_LAMBDA_ARGS_  , __VA_ARGS__)
+
+#define COUNT_THE_ARGS_AND_THEN_CALL_THE_APPROPRIATE_OVERLOAD(basename, ...)  PASTE(basename, COUNT_MACRO_ARGS(__VA_ARGS__))(__VA_ARGS__)
+
 #define COUNT_MACRO_ARGS(...) COUNT_MACRO_ARGS_(__VA_ARGS__, 5,4,3,2,1)
 #define COUNT_MACRO_ARGS_(x1,x2,x3,x4,x5,THE_ANSWER,...)  THE_ANSWER
 
-#define COUNT_THE_ARGS_AND_THEN_CALL_THE_APPROPRIATE_OVERLOAD(basename, ...)  PASTE(basename, COUNT_MACRO_ARGS(__VA_ARGS__))(__VA_ARGS__)
-#define REVERSE(...) COUNT_THE_ARGS_AND_THEN_CALL_THE_APPROPRIATE_OVERLOAD(REVERSE_, __VA_ARGS__)
 #define PASTE(funcname_base,n) PASTE_(funcname_base,n)
 #define PASTE_(x,y) x ## y
 
