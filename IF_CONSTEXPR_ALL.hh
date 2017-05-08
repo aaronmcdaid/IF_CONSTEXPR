f308dd1d7bea439fca613d7db2ff9c8fccd1275a
Aaron McDaid <aaronmcdaid@gmail.com>
2017-05-08 20:49:42 +0200

    Simple function called 'return_the_first_arg_if_true'

--- IF_CONSTEXPR_ALL.hh
+++ IF_CONSTEXPR_ALL.hh
@@ -1,3 +1,13 @@
 #include "IF_CONSTEXPR.hh"
 
 struct empty_t {};
+
+template<typename T, typename F>
+decltype(auto)  return_the_first_arg_if_true
+                ( std::true_type, T&& t, F&& )
+{ return std::forward<T>(t); }
+
+template<typename T, typename F>
+decltype(auto)  return_the_first_arg_if_true
+                ( std::false_type, T&& , F&& f)
+{ return std::forward<F>(f); }
