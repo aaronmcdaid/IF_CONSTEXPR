997fde727c9eafdeb49a8fba7de8a597516b0061
Aaron McDaid <aaronmcdaid@gmail.com>
2017-05-08 21:44:37 +0200

    Rearrange the macros and the whitespace a bit

--- IF_CONSTEXPR_ALL.hh
+++ IF_CONSTEXPR_ALL.hh
@@ -1,34 +1,27 @@
 #include "IF_CONSTEXPR.hh"
 
-#define IF_CONSTEXPR_ALL(...)                                   \
-                    [&]() ->decltype(auto) {                    \
-                        MAKE_THE_CAPTURE_OBJECT(__VA_ARGS__);   \
-                        HANDLE_CONDITION_AND_THE_REST
+#define IF_CONSTEXPR_ALL(...)                       \
+        [&]() ->decltype(auto) {                    \
+            MAKE_THE_CAPTURE_OBJECT(__VA_ARGS__);   \
+            HANDLE_CONDITION_AND_THE_REST
 
 #define MAKE_THE_CAPTURE_OBJECT(...)                        COUNT_THE_ARGS_AND_THEN_CALL_THE_APPROPRIATE_OVERLOAD(MAKE_THE_CAPTURE_OBJECT_ , __VA_ARGS__)
-#define HANDLE_CONDITION_AND_THE_REST(...)      \
-    constexpr bool condition_variable = (__VA_ARGS__);      HANDLE_FIRST_EXPRESSION_AND_THE_REST
+#define MAKE_THE_CAPTURE_OBJECT_2(var1, var2)       \
+            struct local_copy_of_args_t {           \
+                decltype(var1)  &  var1;            \
+                decltype(var2)  &  var2;            \
+            };                                      \
+            auto local_copy_of_args = local_copy_of_args_t  { var1, var2 }
+
+#define HANDLE_CONDITION_AND_THE_REST(...)          \
+            constexpr bool condition_variable = (__VA_ARGS__);      HANDLE_FIRST_EXPRESSION_AND_THE_REST
 #define HANDLE_FIRST_EXPRESSION_AND_THE_REST(...)           HANDLE_FIRST_EXPRESSION (__VA_ARGS__)       HANDLE_SECOND_EXPRESSION_AND_THE_REST
 #define HANDLE_SECOND_EXPRESSION_AND_THE_REST(...)          HANDLE_SECOND_EXPRESSION(__VA_ARGS__)       FINISH_WITH_THE_COMPUTATION
 
-#define MAKE_THE_CAPTURE_OBJECT_2(var1, var2)  \
-        struct local_copy_of_args_t {          \
-            decltype(var1)  &  var1;           \
-            decltype(var2)  &  var2;           \
-        };                                     \
-        auto local_copy_of_args = local_copy_of_args_t  { var1, var2 }
-
-#define HANDLE_FIRST_EXPRESSION(...)    auto first_expression_lambda    =   LAMBDA_TO_COMPUTE_AN_EXPRESSION(__VA_ARGS__);
-#define HANDLE_SECOND_EXPRESSION(...)   auto second_expression_lambda   =   LAMBDA_TO_COMPUTE_AN_EXPRESSION(__VA_ARGS__);
-#define FINISH_WITH_THE_COMPUTATION                                 \
-        return                                                      \
-            return_the_first_arg_if_true                            \
-            (   std::integral_constant<bool, condition_variable>{}  \
-            ,    first_expression_lambda                            \
-            ,   second_expression_lambda                            \
-            )                                                       \
-            ( std::move(local_copy_of_args), empty_t{} );           \
-    }()
+#define HANDLE_FIRST_EXPRESSION(...)                \
+            auto first_expression_lambda    =   LAMBDA_TO_COMPUTE_AN_EXPRESSION(__VA_ARGS__);
+#define HANDLE_SECOND_EXPRESSION(...)               \
+            auto second_expression_lambda   =   LAMBDA_TO_COMPUTE_AN_EXPRESSION(__VA_ARGS__);
 
 #define LAMBDA_TO_COMPUTE_AN_EXPRESSION(...) [](auto && local_copy_of_args, auto && dummy) ->decltype(auto) {   \
             struct one_expression_t : local_copy_of_args_t, std::remove_reference_t<decltype(dummy)> {          \
@@ -39,6 +32,16 @@
             };                                                                                                  \
             return one_expression_t( std::move(local_copy_of_args) ) . go();                                    \
         }
+
+#define FINISH_WITH_THE_COMPUTATION                                 \
+        return                                                      \
+            return_the_first_arg_if_true                            \
+            (   std::integral_constant<bool, condition_variable>{}  \
+            ,    first_expression_lambda                            \
+            ,   second_expression_lambda                            \
+            )                                                       \
+            ( std::move(local_copy_of_args), empty_t{} );           \
+        }()
 
 
 struct empty_t {};
